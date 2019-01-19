e140e868f74fbf44f8d691751dcd01953dd59389
mabb0512 <mariana.bravo@gmail.com>
2019-01-18 20:41:40 -0500

    Updates

---
+++ precache-manifest.4733184a72cf1cacbda6b806cc0398b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "4bb0595be420763fc066",
+    "url": "/clicky-game/static/js/main.4bb0595b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4bb0595be420763fc066",
+    "url": "/clicky-game/static/css/main.9730a44e.chunk.css"
+  },
+  {
+    "revision": "63869f753293e0c7505663ac4fb7ab05",
+    "url": "/clicky-game/index.html"
+  }
+];
